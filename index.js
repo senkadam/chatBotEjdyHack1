64a16915934c34d2ad59b5ff77888d2d290583bc
Adam Senk <senkadam@gmail.com>
2016-10-24 00:17:53 +0200

    Ovcobot

--- index.js
+++ index.js
@@ -17,20 +17,21 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 // Index route
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
+    persistentMenu();
     res.send('Hello world, I am a chat bot')
 })
 
 // for Facebook verification
-app.get('/webhook/', function (req, res) {
+app.get('/webhook/', function(req, res) {
     if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
 })
 
-app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
+app.post('/webhook/', function(req, res) {
+    let messaging_events = req.body.entry[0].messaging;
     for (let i = 0; i < messaging_events.length; i++) {
         let event = req.body.entry[0].messaging[i]
         let sender = event.sender.id
@@ -40,12 +41,19 @@ app.post('/webhook/', function (req, res) {
                 sendGenericMessage(sender)
                 continue
             }
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+            quickReply(sender);
         }
         if (event.postback) {
-            let text = JSON.stringify(event.postback)
-            sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
-            continue
+
+
+            let payload = event.postback.payload;
+            if(payload === "HERMAN"){
+                videoBrady(sender)
+            } else if (payload === "JAK_JSEM_HRDY"){
+                videoJsemHrdy(sender);
+            } else {
+                quickReply(sender);
+            }
         }
     }
     res.sendStatus(200)
@@ -54,13 +62,13 @@ app.post('/webhook/', function (req, res) {
 const token = "EAARnBYwTpAYBALZAvupQEh7A6bPRDwyVV2Fbd9OZAgFFVKYhwrK7ERf7D3V24A0j1KdXnsH2bhEOqBbSwOp7QmB3DVPXJqkJ9m6pWwwBEyVWs0VlLRdoBcCUoSZBfHaxVegsvwY9Fv4RIuikjf9x1SjFUvGisDvyHJMat8OjAZDZD";
 
 function sendTextMessage(sender, text) {
-    let messageData = { text:text }
+    let messageData = {text: text}
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
+        qs: {access_token: token},
         method: 'POST',
         json: {
-            recipient: {id:sender},
+            recipient: {id: sender},
             message: messageData,
         }
     }, function(error, response, body) {
@@ -105,13 +113,13 @@ function sendGenericMessage(sender) {
                 }]
             }
         }
-    }
+    };
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
+        qs: {access_token: token},
         method: 'POST',
         json: {
-            recipient: {id:sender},
+            recipient: {id: sender},
             message: messageData,
         }
     }, function(error, response, body) {
@@ -123,6 +131,124 @@ function sendGenericMessage(sender) {
     })
 }
 
+function videoBrady(sender) {
+    let messageData = {
+        "attachment": {
+            "type": "video",
+            "payload": {
+                "url": "https://www.facebook.com/bernyz/videos/10210962774648056/"
+            }
+        }
+    };
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: token},
+        method: 'POST',
+        json: {
+            recipient: {id: sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+};
+
+function videoJsemHrdy(sender) {
+    let messageData = {
+        text: "Na Milose jsem opravdu hrdy, jen se podivej: https://www.youtube.com/watch?v=EcSUOOVpM6Y"
+    };
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: token},
+        method: 'POST',
+        json: {
+            recipient: {id: sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+};
+
+function quickReply(sender) {
+    let messageData = {
+        "text": "Tato otazka jasne ukazuje, ze i u Vas doslo k fasizaci a jednoznacne jste se stal clenem prazske kavarny. " +
+        "Proto se nebudu vyjadřovat. Ale můžu se vajádřit k následujícím tématům:",
+        "quick_replies": [
+            {
+                "content_type": "text",
+                "title": "Jak jsem hrdý na Miloše",
+                "payload": "JAK_JSEM_HRDY"
+            },
+            {
+                "content_type": "text",
+                "title": "Herman a jeho skazky ",
+                "payload": "HERMAN"
+            }
+        ]
+    };
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: token},
+        method: 'POST',
+        json: {
+            recipient: {id: sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+};
+
+
+function persistentMenu() {
+    let body = {
+        "setting_type": "call_to_actions",
+        "thread_state": "existing_thread",
+        "call_to_actions": [
+            {
+                "type": "postback",
+                "title": "Na Miloše jsem hrdý",
+                "payload": "JAK_SEM_HRDY"
+            },
+            {
+                "type": "postback",
+                "title": "Hermana nebudu komentovat",
+                "payload": "HERMAN"
+            },
+            {
+                "type": "web_url",
+                "title": "Je prezident jeste na hrade?",
+                "url": "http://www.jetencurakjesteprezidentem.cz/"
+            }
+        ]
+    };
+
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/thread_settings',
+        qs: {access_token: token},
+        method: 'POST',
+        json: body,
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    }
+};
 
 
 // Spin up the server
